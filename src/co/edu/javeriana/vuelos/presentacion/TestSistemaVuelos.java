2fffc16979e120b5b1d319521f13aac7dacb7f2d
Your Name <you@example.com>
2017-03-12 22:27:41 -0500

    Primer commit Nicolas

--- src/co/edu/javeriana/vuelos/presentacion/TestSistemaVuelos.java
+++ src/co/edu/javeriana/vuelos/presentacion/TestSistemaVuelos.java
@@ -5,6 +5,8 @@ public class TestSistemaVuelos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		
+		System.out.println("Nicolas");
 	}
 
 }
